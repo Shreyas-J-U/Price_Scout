88348b8a43d158a0afb098b4db247bdad0fd2e56
Shreyas-J-U <sshreyasju@gmail.com>
2024-11-20 16:41:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './comp/Navbar';
@@ -34,6 +9,7 @@ import TeamPage from './pages/TeamPage';
 import FeedbackPage from './pages/Feedback';
 import LoginPage from './pages/Login';
 import RegisterPage from './pages/Register';
+// import AccountPage from './pages/AccountPage'; // Import the new AccountPage
 
 const App = () => {
   return (
